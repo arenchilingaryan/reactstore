c119188d2b0f2bfb1b4efca2e1a5106d29bb5609
Aren <reactDOM.render@yandex.ru>
2020-04-23 15:00:59 +0300

    Updates

---
+++ precache-manifest.c66230f51c1d0fdd32ab4af73c73c17a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33dbbf5e6e6cc853a235c36353da1a69",
+    "url": "/reactstore/index.html"
+  },
+  {
+    "revision": "31ea22c25630342db8e6",
+    "url": "/reactstore/static/css/main.08337e1e.chunk.css"
+  },
+  {
+    "revision": "60e81715cf1de19a0fef",
+    "url": "/reactstore/static/js/2.5a71e83a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactstore/static/js/2.5a71e83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31ea22c25630342db8e6",
+    "url": "/reactstore/static/js/main.5f6ab7a2.chunk.js"
+  },
+  {
+    "revision": "cffab61f94a097532acb",
+    "url": "/reactstore/static/js/runtime-main.4ac2361b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bookstore/precache-manifest.dbfb7e0756d337bf380a30079de3dbc5.js"
+  "/reactstore/precache-manifest.c66230f51c1d0fdd32ab4af73c73c17a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bookstore/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactstore/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
